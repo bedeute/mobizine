c7ebd23a0b46cae0cc1eec2cf148b962a7a495f9
bedeute <bedeute@gmail.com>
2015-05-04 14:07:02 +0700

    update dark version

--- dist/scripts/head.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Head.JS
- * P.S. Included all scripts, that necessary to place at the head tag, except some specific old IE polyfills
- * Author: bedeute
- * Email: bedeute@gmail.com
- */
-
-
