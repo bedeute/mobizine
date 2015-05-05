0e76454f80b8c0bce3d3d195aa4e6a200f88fd63
bedeute <bedeute@gmail.com>
2015-05-05 20:16:59 +0700

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,12 +7,59 @@
 (function($) {
   'use strict';
 
+  // $(function() {
+  //   $('.navbar-toggle').click(function (e) {
+  //     e.preventDefault();
+  //     $('.mini-toc').toggleClass('hide show');
+  //     $('header.header').toggleClass('navbar-fixed-top');
+  //   });
+  // });
+
+  // $(function() {
+  //   $('.navbar-toggle').click(function (e) {
+  //     e.preventDefault();
+  //     $('.slidebar').toggleClass('shrink reveal');
+  //   });
+  // });
+
   $(function() {
+
+    if ($('.slidebar').hasClass('shrink')) {
+      $('body').css('overflow-x','hidden');
+    };
+
     $('.navbar-toggle').click(function (e) {
       e.preventDefault();
-      $('.mini-toc').toggleClass('hide show');
-      $('header.header').toggleClass('navbar-fixed-top');
+      e.stopPropagation();
+      toggleSlidebar();
     });
   });
+
+  $(document).click(function(event) {
+    if(!$(event.target).closest('.slidebar').length) {
+        if($('.slidebar').hasClass('reveal')) {
+            $('.slidebar').removeClass('reveal');
+            $('.slidebar').addClass('shrink');
+            $('body').removeClass('slidebar-revealed');
+        }
+    }
+})
+
+
+
+function toggleSlidebar() {
+  var slidebar = $('.slidebar');
+  if (slidebar.hasClass('shrink')) {
+    $('body').addClass('slidebar-revealed');
+    slidebar.removeClass('shrink');
+    slidebar.addClass('reveal');
+  } else
+  if (slidebar.hasClass('reveal')) {
+    $('body').removeClass('slidebar-revealed');
+    slidebar.removeClass('reveal');
+    slidebar.addClass('shrink');
+  }
+}
+
 
 })(jQuery);
